2048a87f9ce501563828b7f62dd500c0f1a7bd63
JakeDorne <jakedorne@hotmail.com>
2016-09-17 15:32:12 +1200

    not sure why I have to commit to test this...

--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,19 @@ var router = express.Router();
 
 var pg = require('pg');
 
-//pg.defaults.ssl = true;
-var client = new Client(process.env.DATABASE_URL);
-
+pg.defaults.ssl = true;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  client.query("SELECT * FROM EasyScores")
-  .then((result) => res.render('index', { title: result }))
-
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query('SELECT * FROM EasyScores', function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.render('index', {title: result.rows} ); }
+    });
+  });
 });
 
 module.exports = router;

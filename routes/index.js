15d14f48544e7ece95268c6b3585597e1db113a9
JakeDorne <jakedorne@hotmail.com>
2016-09-17 16:07:09 +1200

    success

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,12 @@ pg.defaults.ssl = true;
 /* GET home page. */
 router.get('/easy', function(req, res, next) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM EasyScores LIMIT 1;', function(err, result) {
+    client.query('SELECT * FROM EasyScores;', function(err, result) {
       done();
       if (err)
        { console.error(err); res.send("Error " + err); }
       else
-       { res.render('index', {title: JSON.stringify(result.rows)} ); }
+       { res.render('index', {title: 'Easy HighScores', result: JSON.stringify(result.rows)} ); }
     });
   });
 });
@@ -25,7 +25,7 @@ router.get('/hard', function(req, res, next) {
       if (err)
        { console.error(err); res.send("Error " + err); }
       else
-       { res.render('index', {title: JSON.stringify(result.rows).name} ); }
+       { res.render('index', {title: 'Hard Highscores', result: JSON.stringify(result.rows)} ); }
     });
   });
 });

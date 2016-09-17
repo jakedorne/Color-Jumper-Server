2c3f8daa8f9c9755f4525cfbbe33cd4392d548f0
JakeDorne <jakedorne@hotmail.com>
2016-09-17 18:04:33 +1200

    sending information instead of rendering

--- routes/index.js
+++ routes/index.js
@@ -13,7 +13,7 @@ router.get('/easy', function(req, res, next) {
       if (err)
        { console.error(err); res.send("Error " + err); }
       else
-       { res.render('index', {title: 'Easy HighScores', result: JSON.stringify(result.rows)} ); }
+       { res.send(JSON.stringify(result.rows)); }
     });
   });
 });
@@ -25,7 +25,7 @@ router.get('/hard', function(req, res, next) {
       if (err)
        { console.error(err); res.send("Error " + err); }
       else
-       { res.render('index', {title: 'Hard Highscores', result: JSON.stringify(result.rows)} ); }
+       { res.send(JSON.stringify(result.rows)); }
     });
   });
 });

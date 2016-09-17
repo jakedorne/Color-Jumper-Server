564c94949fabdcd32c48b4fd37f48a0b47f4b31f
JakeDorne <jakedorne@hotmail.com>
2016-09-17 15:10:45 +1200

    pls work

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
 var express = require('express');
 var router = express.Router();
 
-// var pg = require('pg');
-// //var database_url = "postgres://uhythsjzloykmc:63mJHqC21tl6m7SJ0WHBhMz4aP@ec2-23-21-238-76.compute-1.amazonaws.com:5432/daq0tlknrgimqq";
-// pg.defaults.ssl = true;
-// pg.connect(process.env.DATABASE_URL, function(err, client) {
-//   if (err) throw err;
-//   console.log('Connected to postgres! Getting schemas...');
-//
-//   client
-//     .query('SELECT * FROM EasyScores;')
-//     .on('row', function(row) {
-//       console.log(JSON.stringify(row));
-//     });
-// });
+var pg = require('pg');
+
+pg.defaults.ssl = true;
+pg.connect(process.env.DATABASE_URL, function(err, client) {
+  if (err) throw err;
+  console.log('Connected to postgres! Getting schemas...');
+
+  client
+    .query('SELECT table_schema,table_name FROM information_schema.tables;')
+    .on('row', function(row) {
+      console.log(JSON.stringify(row));
+    });
+});
 
 
 /* GET home page. */

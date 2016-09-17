2b665c258ba15944da3f3b06ba92b7c77bd70e42
JakeDorne <jakedorne@hotmail.com>
2016-09-17 15:17:59 +1200

    not sure why I have to commit to test this...

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,14 @@ var router = express.Router();
 var pg = require('pg');
 
 pg.defaults.ssl = true;
-pg.connect(process.env.DATABASE_URL, function(err, client) {
-  if (err) throw err;
-  console.log('Connected to postgres! Getting schemas...');
-
-  client
-    .query('SELECT table_schema,table_name FROM information_schema.tables;')
-    .on('row', function(row) {
-      console.log(JSON.stringify(row));
-    });
-});
+client = pg.connect(process.env.DATABASE_URL);
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  client.query("SELECT * FROM EasyScores")
+  .then((result) => res.render('index', { title: result }))
+
 });
 
 module.exports = router;

a97da50317626d7132a3194b380181a7d3b0d2f4
JakeDorne <jakedorne@hotmail.com>
2016-09-18 03:14:48 +1200

    added put methods for API

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ pg.defaults.ssl = true;
 /* GET home page. */
 router.get('/easy', function(req, res, next) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM EasyScores;', function(err, result) {
+    client.query('SELECT * FROM EasyScores ORDER BY Score DESC LIMIT 100;', function(err, result) {
       done();
       if (err)
        { console.error(err); res.send("Error " + err); }
@@ -18,14 +18,41 @@ router.get('/easy', function(req, res, next) {
   });
 });
 
+router.put('/easy', function(req, res, next) {
+  var query = 'INSERT INTO EasyScores VALUES ('+req.body.name+', '+req.body.score+');';
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query(query, function(err, result) {
+      done();
+      if (err)
+       { console.error(err); res.send("Error " + err); }
+      else
+       { res.send(); }
+    });
+  });
+});
+
+
 router.get('/hard', function(req, res, next) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM HardScores;', function(err, result) {
+    client.query('SELECT * FROM HardScores ORDER BY Score DESC LIMIT 100;', function(err, result) {
       done();
       if (err)
        { console.error(err); res.send("Error " + err); }
       else
        { res.send(result.rows); }
+    });
+  });
+});
+
+router.put('/hard', function(req, res, next) {
+  var query = 'INSERT INTO HardScores VALUES ('+req.body.name+', '+req.body.score+');';
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query(query, function(err, result) {
+      done();
+      if (err)
+       { console.error(err); res.send("Error " + err); }
+      else
+       { res.send(); }
     });
   });
 });

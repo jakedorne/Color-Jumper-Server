edd81c4775da9edc61a02d145e12e44f4c2202fa
JakeDorne <jakedorne@hotmail.com>
2016-09-17 14:08:50 +1200

    trying to get connecting to heroku db

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 var pg = require('pg');
 //var database_url = "postgres://uhythsjzloykmc:63mJHqC21tl6m7SJ0WHBhMz4aP@ec2-23-21-238-76.compute-1.amazonaws.com:5432/daq0tlknrgimqq";
-//pg.defaults.ssl = true;
+pg.defaults.ssl = true;
 pg.connect(process.env.DATABASE_URL, function(err, client) {
   if (err) throw err;
   console.log('Connected to postgres! Getting schemas...');

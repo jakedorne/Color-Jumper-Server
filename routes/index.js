8c9fa9e4f23c7e8a29c8ce784063251faef5f218
JakeDorne <jakedorne@hotmail.com>
2016-09-17 15:56:32 +1200

    not sure why I have to commit to test this...

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,12 @@ pg.defaults.ssl = true;
 /* GET home page. */
 router.get('/easy', function(req, res, next) {
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query('SELECT * FROM EasyScores;', function(err, result) {
+    client.query('SELECT * FROM EasyScores LIMIT 1;', function(err, result) {
       done();
       if (err)
        { console.error(err); res.send("Error " + err); }
       else
-       { res.render('index', {title: result.rows.name} ); }
+       { res.render('index', {title: rJSON.stringify(result.rows).name} ); }
     });
   });
 });
@@ -30,6 +30,9 @@ router.get('/hard', function(req, res, next) {
   });
 });
 
+router.get('/', function(res, error){
+  res.render('index', {title: 'Highscores'});
+});
 
 
 module.exports = router;
